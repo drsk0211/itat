7bba52855bcdf852a8ca8f188ded9d6c34f749c7
reesly <reesly@gmail.com>
2020-04-03 14:43:59 +0300

    new vers

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,7 +138,7 @@
           e.preventDefault();
       });
 
-      // ���� ��� SEO �������
+      // ���� ��� SEO �������
       $('[href^="https://www.i-tat.ru/prostaya_smeta/index.html"]').on('click', function(){
         ym(57491743, 'reachGoal', 'fromSeoPages'); 
         return true;
@@ -156,4 +156,124 @@
 
   });      
 
+
+
 }(jQuery));
+
+
+  
+  // <!--  КАЛЬКУЛЯТОР  -->
+  
+  function changeChecked(id){
+    if ($("#"+id).hasClass('lgray')) {
+      $("#"+id).removeClass('lgray');
+      $("#"+id).parent().parent().addClass('strong');
+    }
+    else{
+      $("#"+id).addClass('lgray');
+      $("#"+id).parent().parent().removeClass('strong');
+    }
+    calculate();
+  }
+  
+  number_format = function (number, decimals, dec_point, thousands_sep) {
+    number = number.toFixed(decimals);
+
+    var nstr = number.toString();
+    nstr += '';
+    x = nstr.split('.');
+    x1 = x[0];
+    x2 = x.length > 1 ? dec_point + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+
+    while (rgx.test(x1))
+        x1 = x1.replace(rgx, '$1' + thousands_sep + '$2');
+
+    return x1 + x2;
+  }
+
+  var total = 26000;
+  document.getElementById('Totalcost').innerHTML = number_format(total, 0, '.', ' ')+"р.";
+
+  
+
+
+  function calculate() {
+    total=26000;
+    if (!$("#cv2").hasClass('lgray')) {
+      total += parseInt($("#cv2").data('value'));
+    }
+    if (!$("#cv3").hasClass('lgray')) {
+      total += parseInt($("#cv3").data('value'));
+    } 
+    if (!$("#cv4").hasClass('lgray')) {
+      total += parseInt($("#cv4").data('value'));
+    } 
+    if (!$("#cv5").hasClass('lgray')) {
+      total += parseInt($("#cv5").data('value'));
+    } 
+    if (!$("#cv6").hasClass('lgray')) {
+      total += parseInt($("#cv6").data('value'));
+    } 
+    if (!$("#cv7").hasClass('lgray')) {
+      total += parseInt($("#cv7").data('value'));
+    } 
+    if (!$("#cv8").hasClass('lgray')) {
+      total += parseInt($("#cv8").data('value'));
+    } 
+ 
+     
+    
+    //alert(total);
+    document.getElementById('Totalcost').innerHTML = number_format(total, 0, '.', ' ') + "р.";
+  }
+
+  {/* /КАЛЬКУЛЯТОР  */}
+
+
+  
+function request_form_yandex() {
+  
+  total=26000;
+  var tx = '- 26 000 Проф Базовый; '+"\n";
+    if (!$("#cv2").hasClass('lgray')) {
+      total += parseInt($("#cv2").data('value'));
+      tx += "- "+ $("#cv2").parent().parent().text().replace(/\s+/g, " ")+"\n";
+    }
+    if (!$("#cv3").hasClass('lgray')) {
+      total += parseInt($("#cv3").data('value'));
+      tx += "- "+ $("#cv3").parent().parent().text().replace(/\s+/g, " ")+"\n";
+    } 
+    if (!$("#cv4").hasClass('lgray')) {
+      total += parseInt($("#cv4").data('value'));
+      tx += "- "+ $("#cv4").parent().parent().text().replace(/\s+/g, " ")+"\n";
+    } 
+    if (!$("#cv5").hasClass('lgray')) {
+      total += parseInt($("#cv5").data('value'));
+      tx += "- "+ $("#cv5").parent().parent().text().replace(/\s+/g, " ")+"\n";
+    } 
+    if (!$("#cv6").hasClass('lgray')) {
+      total += parseInt($("#cv6").data('value'));
+      tx += "- "+ $("#cv6").parent().parent().text().replace(/\s+/g, " ")+"\n";
+    } 
+    if (!$("#cv7").hasClass('lgray')) {
+      total += parseInt($("#cv7").data('value'));
+      tx += "- "+ $("#cv7").parent().parent().text().replace(/\s+/g, " ")+"\n";
+    } 
+    if (!$("#cv8").hasClass('lgray')) {
+      total += parseInt($("#cv8").data('value'));
+      tx += "- "+ $("#cv8").parent().parent().text().replace(/\s+/g, " ")+"\n";
+    } 
+ 
+  pars="";
+  tx = tx + " ИТОГО: "+ total;
+
+  if(tx != ''){ pars = '&answer_long_text_2038384='+ encodeURIComponent(tx); }
+  
+  
+  pars = 'https:'+'/'+'/forms.yandex.ru/u/5e74adffb1c70e0c2fe645e0/?iframe=1'+pars;
+  document.getElementById('form1').src = pars;
+  
+  ym(61606258, 'reachGoal', 'RequestBill'); 
+  return true;
+}
